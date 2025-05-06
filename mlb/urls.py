2f8fe6d518f62fd5fcb5445dc60c97aaa34ceba6
JP Harris <jpharris.contact@gmail.com>
2025-05-06 17:52:27 -0400

    adds games per day endpoints

--- mlb/urls.py
+++ mlb/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TeamsView, TeamView, PlayersView, PlayerView, GamesView, GameView
+from .views import TeamsView, TeamView, PlayersView, PlayerView, GamesView, GameView, TodaysScheduleView, LastWeekScheduleView
 
 urlpatterns = [
     path('teams/', TeamsView.as_view(), name='teams'),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('players/<int:player_id>/', PlayerView.as_view(), name='player'),
     path('games/', GamesView.as_view(), name='games'),
     path('games/<int:game_id>/', GameView.as_view(), name='game'),
+    path('games_today', TodaysScheduleView.as_view(), name='schedule today'),
+    path('games_last_week', LastWeekScheduleView.as_view(), name='Games in the last week'),
 ] 
--- mlb/views.py
+++ mlb/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import date, datetime, timedelta
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,6 +7,39 @@ import statsapi
 from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+TEAM_IDS = [
+    133,
+    136,
+    139,
+    108,
+    140,
+    141,
+    110,
+    142,
+    111,
+    145,
+    114,
+    147,
+    116,
+    117,
+    118,
+    134,
+    135,
+    137,
+    138,
+    109,
+    143,
+    112,
+    144,
+    113,
+    146,
+    115,
+    119,
+    120,
+    121,
+    158
+]
 
 class MLBBaseView(APIView):
     permission_classes = [IsAuthenticated]
@@ -17,10 +51,12 @@ class MLBBaseView(APIView):
 class TeamsView(MLBBaseView):
     def get(self, request):
         try:
-          
-          return Response(statsapi.get('teams', params={'season': 2024, 'leagueIds': 103}))
-            # teams = self.mlb_api.teams()
-            # return Response(teams)
+          NL = statsapi.get('teams', params={'season': 2024, 'leagueIds': 103})
+          AL = statsapi.get('teams', params={'season': 2024, 'leagueIds': 104})
+
+          teams = NL["teams"] + AL["teams"]
+
+          return Response(teams)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
@@ -65,5 +101,31 @@ class GameView(MLBBaseView):
         try:
             game = self.mlb_api.game(game_id)
             return Response(game)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class TodaysScheduleView(MLBBaseView):
+    def get(self, request):
+        try:
+            games = statsapi.schedule(date=date.today(), team="", opponent="", sportId=1, game_id=None)
+            return Response(games)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class LastWeekScheduleView(MLBBaseView):
+    def get(self, request):
+        try:
+            last_week = [
+                (date.today() - timedelta(days=i)).strftime('%Y-%m-%d')
+                for i in range(7)
+            ]
+
+            games = []
+
+            for day in last_week:
+                daily_games = statsapi.schedule(date=day, team="", opponent="", sportId=1, game_id=None)
+                games.append(daily_games)
+            
+            return Response(games)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
